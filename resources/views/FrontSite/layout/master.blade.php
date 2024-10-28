c866a251c7883326d8e0d2f7f9ff83d34e1f4074
Mohamed Mahrous <etos7plus@gmail.com>
2024-10-28 15:06:49 +0300

    tamplate part 1

---
+++ resources/views/FrontSite/layout/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('FrontSite.layout.header')
+
+<body>
+    @yield('content')
+</body>
+
+@include('FrontSite.layout.footer')
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/',  'index');
+    Route::post('storeAds',  'storeAds')->name('storeAds');
+    Route::post('resumeAds',  'resumeAds')->name('resumeAds');
+    Route::post('deleteAds',  'deleteAds')->name('deleteAds');
+    Route::get('StoreAdsView',  'StoreAdsView')->name('StoreAdsView');
+    Route::get('StopAds/{id}',  'StopAds');
 });
